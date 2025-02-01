4f8f44e57d891a3f3ea6395de833457d55880d45
Muhamad Fauzan <86282039+mfauzan1202@users.noreply.github.com>
2025-02-01 14:02:42 +0800

    commit for minimum-setup

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
-use App\Http\Controllers\ItemController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [ItemController::class, 'index'])->name('items.index');
-Route::get('/items/create', [ItemController::class, 'create'])->name('items.createItems');
-Route::post('/items', [ItemController::class, 'store'])->name('items.store');
-Route::get('/items/{id}/edit', [ItemController::class, 'edit'])->name('items.editItems');
-Route::put('/items/{id}', [ItemController::class, 'update'])->name('items.update');
-Route::delete('/items/{id}', [ItemController::class, 'destroy'])->name('items.destroy');
+Route::get('/', function () {
+    return view('welcome');
+});
